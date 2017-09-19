6d68814d10ef59bf9a7e34cdd96c58df291e4251
michaelrojas17 <rojas.vargas.michael@gmail.com>
2017-09-19 21:04:55 +0200

    Revert "Version integrada del programa para ESP12F"
    This reverts commit 89604d65d399ecb624c50d2af3f6abd55d7d55e9.

--- prueba_escritura_memoria_ssid/prueba_escritura_memoria_ssid.ino
+++ prueba_escritura_memoria_ssid/prueba_escritura_memoria_ssid.ino
@@ -9,12 +9,31 @@ unsigned long previousMillis = 0;
 char ssid[50];      
 char pass[50];
 
-const char *ssidConf = "ESP-LEDS-WiFi";
+const char *ssidConf = "tutorial";
 const char *passConf = "12345678";
 
 String mensaje = "";
 
+//-----------CODIGO HTML PAGINA DE CONFIGURACION---------------
+String pagina = "<!DOCTYPE html>"
+"<html>"
+"<head>"
+"<title>Tutorial Eeprom</title>"
+"<meta charset='UTF-8'>"
+"</head>"
+"<body>"
+"</form>"
+"<form action='guardar_conf' method='get'>"
+"SSID:<br><br>"
+"<input class='input1' name='ssid' type='text'><br>"
+"PASSWORD:<br><br>"
+"<input class='input1' name='pass' type='password'><br><br>"
+"<input class='boton' type='submit' value='GUARDAR'/><br><br>"
+"</form>"
+"<a href='escanear'><button class='boton'>ESCANEAR</button></a><br><br>";
 
+String paginafin = "</body>"
+"</html>";
 
 //------------------------SETUP WIFI-----------------------------
 void setup_wifi() {
@@ -23,38 +42,61 @@ void setup_wifi() {
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED and contconexion <50) { //Cuenta hasta 50 si no se puede conectar lo cancela
     ++contconexion;
-    delay(500);
-    
+    delay(250);
+    Serial.print(".");
+    digitalWrite(13, HIGH);
+    delay(250);
+    digitalWrite(13, LOW);
   }
   if (contconexion <50) {   
       Serial.println("");
       Serial.println("WiFi conectado");
       Serial.println(WiFi.localIP());
-     }
+      digitalWrite(13, HIGH);  
+  }
   else { 
       Serial.println("");
       Serial.println("Error de conexion");
+      digitalWrite(13, LOW);
   }
 }
 
 //--------------------------------------------------------------
 WiFiClient espClient;
 ESP8266WebServer server(80);
+//--------------------------------------------------------------
+
+//-------------------PAGINA DE CONFIGURACION--------------------
+void paginaconf() {
+  server.send(200, "text/html", pagina + mensaje + paginafin); 
+}
 
 //--------------------MODO_CONFIGURACION------------------------
 void modoconf() {
    
+  delay(100);
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
 
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssidConf,passConf);
 
+  //WiFi.softAP(ssidConf, passConf);
   IPAddress myIP = WiFi.softAPIP(); 
   Serial.print("IP del acces point: ");
   Serial.println(myIP);
   Serial.println("WebServer iniciado...");
 
+  server.on("/", paginaconf); //esta es la pagina de configuracion
+
   server.on("/guardar_conf", guardar_conf); //Graba en la eeprom la configuracion
 
+  server.on("/escanear", escanear); //Escanean las redes wifi disponibles
   
   server.begin();
 
@@ -71,8 +113,8 @@ void guardar_conf() {
   Serial.println(server.arg("pass"));
   grabar(50,server.arg("pass"));
 
-  mensaje = "Configuracion Guardada..";
-  server.send(200, "text/html",  mensaje);
+  mensaje = "Configuracion Guardada...";
+  paginaconf();
 }
 
 //----------------Función para grabar en la EEPROM-------------------
@@ -102,6 +144,30 @@ String leer(int addr) {
    return strlectura;
 }
 
+//---------------------------ESCANEAR----------------------------
+void escanear() {  
+  int n = WiFi.scanNetworks(); //devuelve el número de redes encontradas
+  Serial.println("escaneo terminado");
+  if (n == 0) { //si no encuentra ninguna red
+    Serial.println("no se encontraron redes");
+    mensaje = "no se encontraron redes";
+  }  
+  else
+  {
+    Serial.print(n);
+    Serial.println(" redes encontradas");
+    mensaje = "";
+    for (int i = 0; i < n; ++i)
+    {
+      // agrega al STRING "mensaje" la información de las redes encontradas 
+      mensaje = (mensaje) + "<p>" + String(i + 1) + ": " + WiFi.SSID(i) + " (" + WiFi.RSSI(i) + ") Ch: " + WiFi.channel(i) + " Enc: " + WiFi.encryptionType(i) + " </p>\r\n";
+      //WiFi.encryptionType 5:WEP 2:WPA/PSK 4:WPA2/PSK 7:open network 8:WPA/WPA2/PSK
+      delay(10);
+    }
+    Serial.println(mensaje);
+    paginaconf();
+  }
+}
 
 //------------------------SETUP-----------------------------
 void setup() {
@@ -139,7 +205,7 @@ void loop() {
   pinMode(13, INPUT);
   digitalWrite(14, HIGH);
 
-  if (currentMillis - previousMillis >= 5000) { 
+  if (currentMillis - previousMillis >= 5000) { //envia la temperatura cada 5 segundos
     previousMillis = currentMillis;
     Serial.println("Funcionado...");
     valor = digitalRead(13);
